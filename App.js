3d3013991c697400482f1b7f17096a65696cdaf6
plusbeauxjours <plusbeauxjours@gmail.com>
2017-12-01 18:56:21 +0900

    added ActiviityIndicator

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <ActivityIndicator />
+        <Text>Hello Native Developer now!</Text>
       </View>
     );
   }
