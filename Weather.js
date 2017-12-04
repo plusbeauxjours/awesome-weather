9f145e7015834fffd7be507c02476b3674b8f41f
plusbeauxjours <plusbeauxjours@gmail.com>
2017-12-04 22:25:47 +0900

    added Icons by vertor icons

--- Weather.js
+++ Weather.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { LinearGradient } from 'expo';
+import { Ionicons } from "@expo/vector-icons";
 
 export default class Weather extends Component{
     render(){
-        return (
-            <LinearGradient colors={['#00C6FB', '#005BEA']} style={styles.container}>
-                <View style={styles.upper}>
-                    <Text>Icon here!</Text>
-                    <Text style={styles.temp}>35</Text>
-                </View>
-            <View style={styles.lower}>
-                <Text style={styles.title}>Raining like a MF</Text>
-                <Text style={styles.subtitle}>For more info look outside</Text>
+        return <LinearGradient colors={["#00C6FB", "#005BEA"]} style={styles.container}>
+            <View style={styles.upper}>
+              <Ionicons color='white' size={144} name='ios-rainy' />
+              <Text style={styles.temp}>35</Text>
             </View>
-            </LinearGradient>
-        );
+            <View style={styles.lower}>
+              <Text style={styles.title}>Raining like a MF</Text>
+              <Text style={styles.subtitle}>
+                For more info look outside
+              </Text>
+            </View>
+          </LinearGradient>;
     }
 }
 
@@ -26,10 +27,11 @@ const styles = StyleSheet.create({
   upper: {
     flex: 1,
     alignItems: "center",
+    backgroundColor: "transparent",
     justifyContent: "center"
   },
   temp: {
-    fontSize: 38,
+    fontSize: 48,
     backgroundColor: "transparent",
     color: "white",
     marginTop: 10
