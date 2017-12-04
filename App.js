ef30e143a3d61b1dbff99f1dbda1c6ac0fe6e243
plusbeauxjours <plusbeauxjours@gmail.com>
2017-12-04 23:43:15 +0900

    Error message on loading state instead of console log

--- App.js
+++ App.js
@@ -4,26 +4,31 @@ import Weather from './Weather';
 
 export default class App extends Component {
   state = {
-    isLoaded: false
+    isLoaded: false,
+    error:null
   }
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(
       position => {
         this.setState({
-            isLoaded: true
+          isLoaded: true
           });
+        console.log(position)
         },
         error => {
-          console.log(error);
+          this.state({
+            error:error
+          })
         }
       );
     }
   render() {
-    const { isLoaded } = this.state;
+    const { isLoaded, error } = this.state;
     return <View style={styles.container}>
         <StatusBar hidden={true} />
         {isLoaded ? <Weather /> : <View style={styles.loading}>
             <ActivityIndicator size='large' color='red' />
+            {error ? <Text style={ styles.errorText }>{ error }</Text> : null}
           </View>}
       </View>;
   }
@@ -33,6 +38,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff"
+  },
+  errorText:{
+    color: 'red',
+    backgroundColor: 'transparent'
   },
   loading: {
     flex: 1,
