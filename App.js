23472231e0aa5a6bbd2276237e28915a36612fba
plusbeauxjours <plusbeauxjours@gmail.com>
2020-03-19 12:05:19 +0900

    if there is no permission for location

--- App.js
+++ App.js
@@ -4,9 +4,13 @@ import {
   Text,
   View,
   ActivityIndicator,
-  StatusBar
+  StatusBar,
+  Linking,
+  TouchableOpacity
 } from "react-native";
 import Weather from "./Weather";
+import Constants from "expo-constants";
+import * as IntentLauncher from "expo-intent-launcher";
 
 const API_KEY = "4beff79b54a07b4e04033175087dcbf0";
 
@@ -15,12 +19,18 @@ export default class App extends Component {
     isLoaded: false,
     error: null,
     temperature: null,
-    name: null
+    name: null,
+    lat: null,
+    long: null
   };
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(
       position => {
         this._getWeather(position.coords.latitude, position.coords.longitude);
+        this.setState({
+          lat: position.coords.latitude,
+          long: position.coords.longitude
+        });
       },
       error => {
         this.setState({
@@ -44,27 +54,74 @@ export default class App extends Component {
   };
 
   render() {
-    const { isLoaded, error, temperature, name } = this.state;
-    return (
-      <View style={styles.container}>
-        <StatusBar hidden={true} />
-        {isLoaded ? (
-          <>
-            <Weather
-              weatherName={name}
-              temp={Math.ceil(temperature - 273.15)}
-            />
-          </>
-        ) : (
+    const { isLoaded, error, temperature, name, lat, long } = this.state;
+    if (lat && long && !loading) {
+      return (
+        <View style={styles.container}>
+          <StatusBar hidden={true} />
+          {isLoaded ? (
+            <>
+              <Weather
+                weatherName={name}
+                temp={Math.ceil(temperature - 273.15)}
+              />
+            </>
+          ) : (
+            <View style={styles.loading}>
+              <Text style={styles.loadingText} i>
+                Getting the fukin weather.....
+              </Text>
+              {error ? <Text style={styles.errorText}>{error}</Text> : null}
+            </View>
+          )}
+        </View>
+      );
+    } else {
+      return (
+        <View style={styles.container}>
+          <StatusBar hidden={true} />
           <View style={styles.loading}>
-            <Text style={styles.loadingText} i>
-              Getting the fukin weather.....
-            </Text>
-            {error ? <Text style={styles.errorText}>{error}</Text> : null}
+            {Platform.OS === "ios" ? (
+              <>
+                <Text>
+                  To enable location, tap Open Settings, then tap on Location,
+                  and finally tap on While Using the App.
+                </Text>
+                <TouchableOpacity
+                  style={styles.button}
+                  onPress={() => Linking.openURL("app-settings:")}
+                >
+                  <Text>SETTINGS</Text>
+                </TouchableOpacity>
+              </>
+            ) : (
+              <>
+                <Text>
+                  To enable location, tap Open Settings, then tap on
+                  Permissions, then tap on Location, and finally tap on Allow
+                  only while using the app.
+                </Text>
+                <TouchableOpacity
+                  style={styles.button}
+                  onPress={() => {
+                    const pkg = Constants.manifest.releaseChannel
+                      ? Constants.manifest.android.package
+                      : "host.exp.exponent";
+                    IntentLauncher.startActivityAsync(
+                      IntentLauncher.ACTION_APPLICATION_DETAILS_SETTINGS,
+                      { data: "package:" + pkg }
+                    ),
+                      setLoading(false);
+                  }}
+                >
+                  <Text>SETTINGS</Text>
+                </TouchableOpacity>
+              </>
+            )}
           </View>
-        )}
-      </View>
-    );
+        </View>
+      );
+    }
   }
 }
 
@@ -83,7 +140,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#FDF6AA",
     justifyContent: "center",
     alignItems: "center",
-    paddingLeft: 25
+    padding: 25
   },
   loadingText: {
     textAlign: "center",
@@ -91,5 +148,14 @@ const styles = StyleSheet.create({
     marginBottom: 24,
     color: "#ffb000",
     fontSize: 20
+  },
+  button: {
+    width: 100,
+    height: 30,
+    borderWidth: 1,
+    borderRadius: 3,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 20
   }
 });
